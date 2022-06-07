921d48691b4f5103c51265c5f4d61108e0597df6
Dale Cole <dale.cole2@yahoo.com>
2022-06-07 17:20:56 -0600

    Week 2 spring boot

--- jeep-sales-m/src/test/java/com/promineotech/jeep/controller/fetchJeepTest.java
+++ jeep-sales-m/src/test/java/com/promineotech/jeep/controller/fetchJeepTest.java
@@ -4,15 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
@@ -25,9 +28,12 @@ import com.promineotech.jeep.entity.JeepModel;
                 config = @SqlConfig(encoding = "utf-8"))
 class fetchJeepTest extends FetchJeepTestSupport {
 
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
   @Test
   void testDb() {
-    
+    int numrows = JdbcTestUtils.countRowsInTable(jdbcTemplate, "Customers");
+    System.out.println("num =" + numrows);
   }
 
 @Disabled
